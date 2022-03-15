ae7e8ae5f111b30fb0a579234df68aadf108cbe4
Vinkle-hzt <vinkle-hzt@outlook.com>
2022-03-15 16:21:40 +0800

    添加武器射击相关脚本

---
+++ Assets/Scripts/PlayerAimWeapon.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BiuBiuBoom.Utils;
+
+public class PlayerAimWeapon : MonoBehaviour
+{
+    public event EventHandler OnShoot;
+    public class OnShootEventArgs : EventArgs
+    {
+        public Vector3 gunEndPointPosition;
+        public Vector3 shootPosition;
+    }
+
+    private Transform aimTransform;
+    private Transform aimGunEndPointTransform; // 子弹发射位置
+    private Animator animator;
+
+    private void Awake()
+    {
+        aimTransform = transform.Find("Aim");
+        aimGunEndPointTransform = transform.Find("GunEndPointPosition");
+        animator = aimTransform.Find("Visual").GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        HandleAiming();
+        HandleShooting();
+    }
+
+    private void HandleAiming()
+    {
+        Vector3 mouseWorldPosition = UtilsClass.GetMouseWorldPosition();
+
+        Vector3 aimDirection = (mouseWorldPosition - aimTransform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+
+        Vector3 aimLocalScale = Vector3.one;
+        if (Mathf.Abs(angle) > 90)
+            aimLocalScale.y = -1f;
+        else
+            aimLocalScale.y = 1f;
+
+        aimTransform.localScale = aimLocalScale;
+    }
+
+
+    private void HandleShooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
+            animator.SetTrigger("Shoot");
+            OnShoot?.Invoke(this, new OnShootEventArgs
+            {
+                gunEndPointPosition = aimGunEndPointTransform.position,
+                shootPosition = mousePosition
+            });
+        }
+    }
+}
---
+++ Assets/Scripts/UtilsClass.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BiuBiuBoom.Utils
+{
+    public class UtilsClass
+    {
+        // Get Mouse Position in World with Z = 0f
+        public static Vector3 GetMouseWorldPosition()
+        {
+            Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+            vec.z = 0f;
+            return vec;
+        }
+
+        public static Vector3 GetMouseWorldPositionWithZ()
+        {
+            return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        }
+
+        public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+        {
+            return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+        }
+
+        public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+        {
+            Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+            return worldPosition;
+        }
+
+        public static Vector3 GetDirToMouse(Vector3 fromPosition)
+        {
+            Vector3 mouseWorldPosition = GetMouseWorldPosition();
+            return (mouseWorldPosition - fromPosition).normalized;
+        }
+    }
+}
