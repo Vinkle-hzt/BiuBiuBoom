df93ae69b01d6383a34872366b5479d493d90c82
TracerChen <2216056290@qq.com>
2022-04-24 10:57:25 +0800

    瘫痪时移动 攻击方法的调用

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Enemy : MonoBehaviour
 {
@@ -14,11 +15,15 @@ public class Enemy : MonoBehaviour
     private bool isFallDown;
 
     private float curTime;
+    private EnemyState state;
 
     private void Awake()
     {
         eInfo = GetComponent<InfoController>();
         enemyInfo = GetComponent<EnemyInfoController>();
+
+        //正常状态
+        state = EnemyState.normal;
     }
 
     // Start is called before the first frame update
@@ -31,8 +36,36 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         FallDown();
+        (state == EnemyState.normal ? (Action)NormalMovement : FallDownAttack)();
+        (state == EnemyState.normal ? (Action)NormalAttack : FallDownAttack)();
     }
 
+    #region 移动
+    void NormalMovement()
+    {
+        //AI移动
+        //不同敌人有不同的移动方法，此处为父类，实现为空方法
+    }
+
+    void FallDownMovement()
+    {
+        //TODO:瘫痪统一不能移动
+    }
+    #endregion
+
+    #region 攻击
+    void NormalAttack()
+    {
+        //同移动为空方法
+    }
+
+    void FallDownAttack()
+    {
+        //同瘫痪移动
+    }
+    #endregion
+
+    #region 瘫痪与重启
     //瘫痪
     void FallDown()
     {
@@ -42,7 +75,8 @@ public class Enemy : MonoBehaviour
             {
                 curTime = enemyInfo.characterData.fallDownTime;
                 isFallDown = true;
-                //TODO: 瘫痪的表现
+                state = EnemyState.fallDown;
+                //TODO: 瘫痪的美术表现
                 //用枪消失替代美术表现
                 transform.Find("Aim").gameObject.SetActive(false);
             }
@@ -50,6 +84,7 @@ public class Enemy : MonoBehaviour
             if (curTime <= 0)
             {
                 isFallDown = false;
+                state = EnemyState.normal;
                 //重启
                 Resume();
             }
@@ -67,4 +102,5 @@ public class Enemy : MonoBehaviour
         enemyInfo.Rusume();
         eInfo.Resume(eInfo.characterData.maxHealth * enemyInfo.characterData.resumePercent);
     }
+    #endregion
 }
