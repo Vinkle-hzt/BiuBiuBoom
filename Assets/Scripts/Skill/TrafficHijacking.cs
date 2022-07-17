98d27af442cc9359a735dd0efaa4811479e9c069
Vinkle-hzt <vinkle-hzt@outlook.com>
2022-07-17 16:50:40 +0800

    添加流量劫持

---
+++ Assets/Scripts/Skill/TrafficHijacking.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BiuBiuBoom.Utils;
+
+public class TrafficHijacking : Skill
+{
+    public Transform pfBullet;
+    public TrafficHijacking(Transform transform) : base(transform)
+    {
+        base.level = 1;
+        pfBullet = Resources.Load<Transform>("Prefabs/Skill/TrafficHijackingBullet");
+    }
+
+    public override void SkillActive()
+    {
+        Vector3 mouseWorldPosition = UtilsClass.GetMouseWorldPosition();
+        Vector3 aimDirection = (mouseWorldPosition - transform.position).normalized;
+
+        Transform bulletTransform = Object.Instantiate(pfBullet, transform.position, Quaternion.identity);
+
+        bulletTransform.GetComponent<TrafficHijackingBullet>().Setup(aimDirection);
+    }
+}
---
+++ Assets/Scripts/Skill/TrafficHijackingBullet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BiuBiuBoom.Utils;
+
+public class TrafficHijackingBullet : MonoBehaviour
+{
+    private Vector3 shootDir;
+
+    [SerializeField]
+    [Header("移动速度")]
+    private float shootSpeed;
+
+    [SerializeField]
+    [Header("变大倍率")]
+    private float scaleRate;
+
+    [SerializeField]
+    [Header("硬直时间")]
+    private float stillTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += shootDir * shootSpeed * Time.deltaTime;
+        transform.localScale += new Vector3(scaleRate, scaleRate, 0) * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "EnemyBullet":
+                Destroy(collision.gameObject);
+                break;
+            case "Enemy":
+                collision.GetComponent<InfoController>().AddBuff(new Stagger(stillTime));
+                break;
+        }
+    }
+
+    public void Setup(Vector3 shootDir)
+    {
+        this.shootDir = shootDir;
+        transform.eulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(shootDir));
+        Destroy(gameObject, 5f);
+    }
+}
