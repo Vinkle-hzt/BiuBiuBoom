3fafc25025239951ca70487c316ebcf20f9c2bf1
Vinkle-hzt <vinkle-hzt@outlook.com>
2022-07-23 12:30:14 +0800

    add FDS and PDS

---
+++ Assets/Scripts/Enemy/FinanceDepartmentStaff/FinanceDepartmentStaff.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinanceDepartmentStaff : Enemy
+{
+    enum Direction
+    {
+        Point1,
+        Point2
+    }
+    private Transform body;
+    private Transform point1;
+    private Transform point2;
+    private Direction direction = Direction.Point1;
+    public Transform target;
+    public float shootCurTime;
+
+    // TODO：更改子弹
+    [Header("子弹")]
+    [SerializeField]
+    private Transform pfBullet;
+    private void Awake()
+    {
+        point1 = transform.Find("Point1");
+        point2 = transform.Find("Point2");
+        body = transform.Find("Body");
+    }
+
+    override protected void NormalMovement()
+    {
+        if (direction == Direction.Point1)
+        {
+            body.position = Vector3.MoveTowards(body.position, point2.position, eInfo.Speed * Time.deltaTime);
+            if (Vector3.Distance(body.position, point2.position) < 0.1f)
+            {
+                direction = Direction.Point2;
+                // TODO 动画
+            }
+        }
+        else
+        {
+            body.position = Vector3.MoveTowards(body.position, point1.position, eInfo.Speed * Time.deltaTime);
+            if (Vector3.Distance(body.position, point1.position) < 0.1f)
+            {
+                direction = Direction.Point1;
+                // TODO 动画
+            }
+        }
+    }
+
+    protected override void NormalAttack()
+    {
+        shootCurTime += Time.deltaTime;
+        if (shootCurTime > 1 / eInfo.AttackSpeed)
+        {
+            shootCurTime = 0;
+            Attack();
+            // TODO: 攻击音乐
+        }
+    }
+
+    void Attack()
+    {
+        target = findTarget();
+
+        if (target == null)
+            return;
+
+        Vector3 shootDir = (target.position - body.position).normalized;
+
+        // 向三个方向发射子弹
+        float cos = Mathf.Cos(5f / 180 * Mathf.PI);
+        float sin = Mathf.Sin(5f / 180 * Mathf.PI);
+
+        Matrix4x4 mat1 = new Matrix4x4(
+            new Vector4(cos, -sin, 0, 0),
+            new Vector4(sin, cos, 0, 0),
+            new Vector4(0, 0, 1, 0),
+            new Vector4(0, 0, 0, 1)
+        );
+        Matrix4x4 mat2 = new Matrix4x4(
+            new Vector4(cos, sin, 0, 0),
+            new Vector4(-sin, cos, 0, 0),
+            new Vector4(0, 0, 1, 0),
+            new Vector4(0, 0, 0, 1)
+        );
+        Vector4 tmp = new Vector4(shootDir.x, shootDir.y, shootDir.z, 1);
+        Vector4 dir1 = mat1.MultiplyVector(tmp);
+        Vector4 dir2 = mat2.MultiplyVector(tmp);
+
+        Transform bullet1 = Instantiate(pfBullet, body.position, Quaternion.identity);
+        Transform bullet2 = Instantiate(pfBullet, body.position, Quaternion.identity);
+        Transform bullet3 = Instantiate(pfBullet, body.position, Quaternion.identity);
+
+        bullet1.GetComponent<Bullet>().Setup(new Vector3(dir1.x, dir1.y, 0), eInfo);
+        bullet2.GetComponent<Bullet>().Setup(new Vector3(dir2.x, dir2.y, 0), eInfo);
+        bullet3.GetComponent<Bullet>().Setup(shootDir, eInfo);
+    }
+}
---
+++ Assets/Scripts/Enemy/ProductionDepartmentStaff/ProductionDepartmentStaff.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductionDepartmentStaff : Enemy
+{
+    enum State
+    {
+        Move,   // 移动
+        Charge, // 蓄力
+        Attack // 攻击
+    }
+    [Header("攻击距离")]
+    public float attackDistance;
+
+    [Header("冲刺距离")]
+    public float flashDistance;
+
+    public Transform target;
+    private float chargeTime;
+    private State curState = State.Move;
+    private float moveDistance;
+    private float moveDir;
+    protected override void NormalMovement()
+    {
+        target = findTarget();
+
+        if (target != null)
+        {
+            switch (curState)
+            {
+                case State.Move:
+                    if (Vector3.Distance(transform.position, target.position) > attackDistance)
+                    {
+                        moveDir = target.position.x - transform.position.x > 0 ? 1 : -1;
+                        transform.position += new Vector3(moveDir * eInfo.Speed * Time.deltaTime, 0, 0);
+                    }
+                    else
+                    {
+                        chargeTime = 0;
+                        curState = State.Charge;
+                    }
+                    break;
+                case State.Charge:
+                    chargeTime += Time.deltaTime;
+                    if (chargeTime >= 1 / eInfo.AttackSpeed)
+                    {
+                        moveDistance = flashDistance;
+                        curState = State.Attack;
+                        // TODO: 蓄力动画，蓄力音效
+                    }
+                    break;
+                case State.Attack:
+                    Attack();
+                    if (moveDistance <= 0)
+                    {
+                        curState = State.Move;
+                        chargeTime = 0;
+                    }
+                    break;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        float curMove = Mathf.Min(moveDir * eInfo.Speed * Time.deltaTime * 10, moveDistance);
+        moveDistance -= Mathf.Abs(curMove);
+        transform.position += new Vector3(curMove, 0, 0);
+        // TODO: 攻击动画，攻击音效
+        // TODO: 攻击判定
+    }
+}
